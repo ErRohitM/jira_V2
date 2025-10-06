75b3f66eae6c307dca82df623fe3a745cc90059b
rohit <majaganvirohit84311@gmail.com>
2025-10-07 02:38:26 +0530

    added Background jobs, reminders for overdue tasks

--- notifications/signals.py
+++ notifications/signals.py
@@ -2,14 +2,16 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from core.models import Tasks
+from users.models import BaseUser
+from .models import NotificationPreference
 from .services import notification_service
 
 
-# @receiver(post_save, sender=User)
-# def create_notification_preferences(sender, instance, created, **kwargs):
-#     """Create notification preferences for new users"""
-#     if created:
-#         NotificationPreference.objects.create(user=instance)
+@receiver(post_save, sender=BaseUser)
+def create_notification_preferences(sender, instance, created, **kwargs):
+    """Create notification preferences for new users"""
+    if created:
+        NotificationPreference.objects.create(user=instance)
 
 
 # @receiver(post_save, sender=Tasks)
